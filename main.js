3f69bad6bc20969aef6847604fcf00f8649cfb00
zondimaqina <zondimaqina0@gmail.com>
2024-04-20 15:00:27 +0200

    add pseudocode

---
+++ main.js
@@ -0,0 +1,11 @@
+/* 
+User must enter 2 inputs => Bill and Tip ;
+User must choose percentage of tip (function) ;
+
+AFTER
+
+user must see amount of bill for each person ;
+user must see amount total amount with bill for each person after pressing tip amount ;
+
+
+*/
