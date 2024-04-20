48f2ea9445d8d80e4fb662052a702d168dc056dc
zondimaqina <zondimaqina0@gmail.com>
2024-04-20 17:17:47 +0200

    store all necessary nodes

--- main.js
+++ main.js
@@ -7,5 +7,25 @@ AFTER
 user must see amount of bill for each person ;
 user must see amount total amount with bill for each person after pressing tip amount ;
 
-
 */
+
+/* TARGET NODES */
+
+let billAmount = 
+document.getElementsByClassName("bill-input") ;
+
+let amountOfPeople =
+document.getElementsByClassName("people")
+
+let tipAmount = 
+document.querySelectorAll(".digit") ;
+
+let resetButton = document.getElementsByClassName("button") ;
+
+
+
+
+
+
+
+
